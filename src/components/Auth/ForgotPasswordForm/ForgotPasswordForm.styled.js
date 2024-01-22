35e99ad4769adf198db5dc8e03f06a8c7df38844
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-01-22 12:29:29 +0000

    Deploying to gh-pages from @ StudentVlad5/bloomskill@d418d4d7055cc5a9e9f6c4fa09f4cbc6973f084c 🚀

--- src/components/Auth/ForgotPasswordForm/ForgotPasswordForm.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { FormTitle, StyledForm } from '../AuthForm.styled';
-
-export const TitleRegister = styled(FormTitle)``;
-
-export const FormRegister = styled(StyledForm)``;
